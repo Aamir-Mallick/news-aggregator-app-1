1924bffb6d5d5cf490084cecd37956cede9f2bcf
Shubham <123shubhamraut@gmail.com>
2020-07-26 12:02:46 +0530

    data fetching operations

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,47 @@
+
 document.addEventListener('DOMContentLoaded',()=> {
  
     // variables
     const toggleSwitch = document.querySelector('#toggle_action');
+    const apiKey = '8e95294833274be596982168d0d69a89';
+    const article_area = document.querySelector("#news-articles");
     const search = document.querySelector('#search');
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
     let searchTxt = '';
+    let sudoUrl = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=';
 
 
+    const showNews = (response) => {
+        console.log('got data');
+        console.log(response);
+
+        if(response.totalResults>0){
+            response.articles.forEach(news=>{
+                console.log(news);
+            });
+        }
+    };
 
 
     // fetching news
     const fetchNews = async (searchTxt) => {
         console.log('loading news');
-
+        try {
+            if (searchTxt) {
+                sudoUrl = 'https://newsapi.org/v2/everything?q=' + searchTxt + '&apiKey=';
+            }
+            let url =proxyUrl+sudoUrl+apiKey;
+            console.log(url);
+            const res = await fetch(url);
+            if (!res.ok) {
+                throw new Error(res.status);
+            }
+            const data = await res.json();
+            showNews(data);
+        }
+        catch (error) {
+            alert(error);
+        }
     };
 
     const getSearch = (event) => {
