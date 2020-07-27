0c06e69747dba8c6b8782a2929e0245aaefc042e
Shubham <123shubhamraut@gmail.com>
2020-07-27 15:55:46 +0530

    news article displayed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,76 +1,121 @@
+document.addEventListener('DOMContentLoaded', () => {
 
-document.addEventListener('DOMContentLoaded',()=> {
- 
-    // variables
-    const toggleSwitch = document.querySelector('#toggle_action');
-    const apiKey = '8e95294833274be596982168d0d69a89';
-    const article_area = document.querySelector("#news-articles");
-    const search = document.querySelector('#search');
-    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-    let searchTxt = '';
-    let sudoUrl = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=';
+  // variables
+  const toggleSwitch = document.querySelector('#toggle_action');
+  const apiKey = '8e95294833274be596982168d0d69a89';
+  const article_area = document.querySelector("#news-articles");
+  const search = document.querySelector('#search');
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  let searchTxt = '';
+  let sudoUrl = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=';
+
+  // Function Showing News
+  const showNews = (response) => {
+    let output = "";
+
+    if (response.totalResults > 0) {
+      response.articles.forEach(news => {
+        output +=
+          ` <li class="article"><a class="article-link" href="${news.url}" >
+                  <div class="img_area">
+                  <img src="${news.urlToImage}" class="article-img" alt="${news.title}"></img>
+                  </div>
+                  <h2 class="article-title">${news.title}</h2>
+                  <p class="article-description">${news.description || "Description not available"}</p>
+                  <span class="article-author">-${news.author ? news.author : "Anon"}</span>
+                  </a>
+                  </li>`;
+      });
+      article_area.innerHTML = output;
+    }
+    else {
+      article_area.innerHTML = '<li class="not-found">No article was found based on the search.</li>';
+    }
+  };
+
+  // fetching news
+  const fetchNews = async (searchTxt) => {
+
+    // for showing loading menu
+    let dotLen = 0;
+    let loadTxt = 'Loading News';
+    let dots = setInterval(() => {
+      console.log('loaddots');
+      if (dotLen <= 3) {
+        article_area.innerHTML = `<p class="load">${loadTxt}</p>`;
+        loadTxt += '.';
+        dotLen += 1;
+      }
+      else {
+        dotLen = 0;
+        loadTxt = 'Loading News';
+      }
+    }, 200);
+
+    // // fetching data
+    // try {
+    //   if (searchTxt) {
+    //     sudoUrl = 'https://newsapi.org/v2/everything?q=' + searchTxt + '&apiKey=';
+    //   }
+    //   let url = proxyUrl + sudoUrl + apiKey;
+    //   console.log(url);
+    // const res = await fetch(url);
+
+    // if (!res.ok) {
+    //   throw new Error(res.status);
+    // }
+    // const data = await res.json();
+
+    // clearInterval(dots);    // clearing loading menu
+    // showNews(data);
+    // }
+
+    // // error handling
+    // catch (error) {
+    //   clearInterval(dots);
+    //   article_area.innerHTML = `<p class="load">An error occurred</p><br><p class="load">${error}</p>`;
+    // }
 
 
-    const showNews = (response) => {
-        console.log('got data');
-        console.log(response);
+    function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    
+    console.log("Hello");
+    sleep(4000).then(() => { 
+      clearInterval(dots);
+      showNews(sudoData);
 
-        if(response.totalResults>0){
-            response.articles.forEach(news=>{
-                console.log(news);
-            });
-        }
-    };
+    });
 
+  };
 
-    // fetching news
-    const fetchNews = async (searchTxt) => {
-        console.log('loading news');
-        try {
-            if (searchTxt) {
-                sudoUrl = 'https://newsapi.org/v2/everything?q=' + searchTxt + '&apiKey=';
-            }
-            let url =proxyUrl+sudoUrl+apiKey;
-            console.log(url);
-            const res = await fetch(url);
-            if (!res.ok) {
-                throw new Error(res.status);
-            }
-            const data = await res.json();
-            showNews(data);
-        }
-        catch (error) {
-            alert(error);
-        }
-    };
+// getting search text
+  const getSearch = (event) => {
+    if (event.which === 13 || event.keyCode === 13 || event.key === "Enter") {
+      if (search.value.trim()) {
+        searchTxt = search.value.trim();
+        console.log(searchTxt);
+        fetchNews(searchTxt);
+      }
+    }
+  };
 
-    const getSearch = (event) => {
-        if (event.which === 13 || event.keyCode === 13 || event.key === "Enter") {
-            if (search.value.trim()) {
-                searchTxt = search.value.trim();
-                console.log(searchTxt);
-                fetchNews(searchTxt);
-            }
-        }
-    };
+  // changing theme
+  const toggle_func = (e) => {
+    if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'light');
+      console.log('theme light');
+    }
+    else {
+      document.documentElement.setAttribute('data-theme', 'default');
+      console.log('theme dark');
+    }
+  };
 
-    // changing theme
-    const toggle_func = (e) => {
-        if (e.target.checked) {
-            document.documentElement.setAttribute('data-theme', 'light');
-            console.log('theme light');
-        }
-        else {
-            document.documentElement.setAttribute('data-theme', 'default');
-            console.log('theme dark');
-        }
-    };
-
-
-    // bind event
-    toggleSwitch.addEventListener('change', toggle_func);
-    search.addEventListener('keypress', getSearch);
-    console.log('document ready');
-    fetchNews();
-
+  // bind event
+  toggleSwitch.addEventListener('change', toggle_func);
+  search.addEventListener('keypress', getSearch);
+  console.log('document ready');
+  fetchNews();
 });
